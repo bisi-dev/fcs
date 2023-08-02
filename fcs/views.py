3e33f2af1ba9a88a3854ec185ec4408ab7297ff2
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2023-08-02 01:52:14 +0100

    feat: deploy fcs homepage

---
+++ fcs/views.py
@@ -0,0 +1,8 @@
+# example/views.py
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+   return render(request, "index.html", {})
