19b162eb3d5348081b539942c1e4772332e1b72b
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2023-08-02 10:26:46 +0100

    feat: bootstrap homepage

--- fcs/views.py
+++ fcs/views.py
@@ -9,3 +9,7 @@ from fcs.models import FundCompany
 def index(request):
    fund_companies = FundCompany.objects.all()
    return render(request, "index.html", {'fund_companies': fund_companies})
+
+def investor(request, slug):
+    fund_company = FundCompany.objects.get(cik_id = slug)
+    return render(request, 'investor.html', {'fund_company': fund_company})
