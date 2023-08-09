8a369711c82aea527fefdd3a290e240f13e35d98
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2023-08-09 11:22:37 +0100

    feat: update models

--- fcs/models.py
+++ fcs/models.py
@@ -6,17 +6,12 @@ class FundCompany(models.Model):
 
 class Filling(models.Model):
     cik_id = models.CharField(max_length=10)
-
-    name_of_issuer = models.CharField(max_length=255)
     cusip = models.CharField(max_length=10)
     value = models.DecimalField(max_digits=20, decimal_places=2)
     shares = models.CharField(max_length=100)
-    investment_discretion = models.CharField(max_length=20)
-    voting_info = models.CharField(max_length=100)
     quarter_info = models.CharField(max_length=100, default="NA")
 
 class Issuer(models.Model):
     name = models.CharField(max_length=255)
     cusip = models.CharField(max_length=10)
     ticker = models.CharField(max_length=10)
-    
